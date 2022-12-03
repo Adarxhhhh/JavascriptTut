ef91239ad50172aaaecb2dadc68ce25d07fcf540
Adarsh220499 <katigiadarsh@gmail.com>
2022-12-02 23:14:03 -0500

    Operators and conditional statements

---
+++ operators_and_ConditionalStatements/script.js
@@ -0,0 +1,24 @@
+// This is a comment section and will not be executed by your interpreter
+
+// Comments are used to maintain documentation
+
+// This is just a complete walkthrough and basic mathsmade for the sake of my very own javascript tutorial
+ // We are learning the Arithmetic operators
+
+ let a = 5;
+ let b = 10;
+
+ console.log("a+b = " , a+b);
+
+ // Simnilarly for the other operators
+
+ // +, - , * , / are your normal Arithmetic operators
+ 
+// % is modulus -- gives you the remainder of a division
+
+// ** is the exponentiation operator --- gives you the power of something ex: a ** b == a to the power b
+
+// ++ increments the given variable
+
+// -- decrements the given variable
+
