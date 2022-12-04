a13b5e2b6e196d3a0b2cb4b4f436eefefac96d24
Adarsh220499 <katigiadarsh@gmail.com>
2022-12-03 22:42:08 -0500

    Loops and Strings

---
+++ Loops and Strings/Loops.js
@@ -0,0 +1,23 @@
+//Loops are used to iterate till a condition is met
+
+//for loop
+
+let sum = 0;
+
+for(let i=0; i<=5;i++){
+    console.log("Aadarsh Katigi");
+
+    let count = i;
+
+    console.log(count);
+
+    sum = sum + i;
+}
+
+console.log(" Sum of the loop is equal to : ",sum);
+
+console.log("Loop has ended");
+
+
+
+
