810f7409c965547a4897dadaf9ef7d42bdfa304e
Adarsh220499 <katigiadarsh@gmail.com>
2022-12-04 19:03:17 -0500

    Changed folder structure

--- first.js
+++
@@ -1,9 +0,0 @@
-fullname  = "Tony Stark";
-age=24;
-price=99.99;
-x=null;
-y=undefined;
-isFollow:true;
-
-let fullName = "Tony Stark";  
-console.log(fullName);
--- intro.js
+++
@@ -1,4 +0,0 @@
-fullName  = "Tony Stark";
-console.log("Adarsh Katigis JS tutorial");
-console.log("Welcome to my JS Tutorial. This will be an extensive and exhaustive one so hang tight!! :)");
-console.log("fullName");
